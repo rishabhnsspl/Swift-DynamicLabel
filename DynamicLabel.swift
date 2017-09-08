dab04c2e180c0d2f959c34973b6e6b5cb3eaf5b0
rishabhnsspl <rishabh1x@gmail.com>
2017-09-08 15:30:05 +0530

    DynamicLabel Class
    A subclass of UILabel written in Swift 3. Automatically calculates label height according to the text string and applies it to the frame property.

---
+++ DynamicLabel.swift
@@ -0,0 +1,43 @@
+//
+//  DynamicLabel.swift
+//  project1
+//
+//  Created by Rishabh on 08/09/17.
+//  Copyright © 2017 Mac4. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DynamicLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func willMove(toSuperview newSuperview: UIView?) {
+        self.makeWithDynamicHeight()
+    }
+    
+    func makeWithDynamicHeight() {
+        self.numberOfLines = 0
+        let newHeight = self.getStringHeight(mytext: self.text!, fontSize: self.font.pointSize, width: self.bounds.width)
+        self.bounds = CGRect(x: 0, y: 0, width: self.bounds.width, height: newHeight)
+    }
+    
+    private func getStringHeight(mytext: String, fontSize: CGFloat, width: CGFloat)->CGFloat {
+        let font = UIFont.systemFont(ofSize: fontSize)
+        let size = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineBreakMode = .byWordWrapping
+        let attributes = [NSFontAttributeName:font, NSParagraphStyleAttributeName:paragraphStyle.copy()]
+        let text = mytext as NSString
+        let rect = text.boundingRect(with: size, options:.usesLineFragmentOrigin, attributes: attributes, context:nil)
+        return rect.size.height
+    }
+    
+}
